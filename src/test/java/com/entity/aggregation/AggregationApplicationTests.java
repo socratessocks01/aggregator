abe66363b5bdfedfeb4ab7dcd8a945fd47911a9a
neo <socratessocks.01@yahoo.com>
2025-01-12 19:57:29 +0530

    Initial commit

---
+++ src/test/java/com/entity/aggregation/AggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entity.aggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
